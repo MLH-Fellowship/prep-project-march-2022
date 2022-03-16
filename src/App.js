08af1930889cb1ae1e375c21dab0adae3770e18c
Siddharth Nair <siddharthmani2000@gmail.com>
2022-03-16 23:19:38 +0530

    Autocomplete Functions

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import { useEffect, useState } from "react";
 import './App.css';
 import logo from './mlh-prep.png'
 
+import PlacesAutocomplete from 'react-places-autocomplete';
+import {
+  geocodeByAddress,
+  geocodeByPlaceId,
+  getLatLng,
+} from 'react-places-autocomplete';
+
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [city, setCity] = useState("New York City")
   const [results, setResults] = useState(null);
+  const [address, setAddress] = useState("New York City");
 
+  const handleSelect = async value => {
+    const results = await geocodeByAddress(value);
+    setAddress(value);
+    setCity(results[0].address_components[0].long_name);
+  };
   useEffect(() => {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + process.env.REACT_APP_APIKEY)
-      .then(res => res.json())
+    .then(res => res.json())
       .then(
         (result) => {
           if (result['cod'] !== 200) {
@@ -34,10 +47,40 @@ function App() {
       <img className="logo" src={logo} alt="MLH Prep Logo"></img>
       <div>
         <h2>Enter a city below 👇</h2>
-        <input
+        {/* <input
           type="text"
           value={city}
-          onChange={event => setCity(event.target.value)} />
+          onChange={event => setCity(event.target.value)} /> */}
+
+          <PlacesAutocomplete
+          value={address}
+          onChange={setAddress}
+          onSelect={handleSelect}
+          >
+            {({getInputProps, suggestions, getSuggestionItemProps, loading}) => (
+            <div>
+            <input {...getInputProps({placeholder: ""})}/>
+            
+            <div>
+              {loading ? <div> Loading </div> : null}
+              {suggestions.map((suggestion) => {
+                const style = {
+                  backgroundColor: suggestion.active? "#41b6e6" : "#fff"
+                };
+                return <div>
+                  <div className="suggestionContainer" {...getSuggestionItemProps(suggestion, {style})}> 
+                <span>{suggestion.description} </span>
+                </div>
+                </div>
+              })}
+            </div>
+
+            </div>
+            )}
+          </PlacesAutocomplete>
+
+
+
         <div className="Results">
           {!isLoaded && <h2>Loading...</h2>}
           {console.log(results)}
