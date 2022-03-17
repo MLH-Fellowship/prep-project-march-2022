cc84afe1e2162c2c907601800a713aa252144986
minh-ctrl <binhminh140899@gmail.com>
2022-03-17 23:47:52 +0900

    feat: 🎸 template to put in chartjs for annica

--- src/App.js
+++ src/App.js
@@ -1,55 +1,98 @@
 import { useEffect, useState } from "react";
-import './App.css';
-import logo from './mlh-prep.png'
+import "./App.css";
+import axios from "axios";
+import Result from "./Components/Result";
+const { REACT_APP_APIKEY } = process.env;
 
+// function App() {
+//   const [error, setError] = useState(null);
+//   const [isLoaded, setIsLoaded] = useState(false);
+//   const [city, setCity] = useState("New York City");
+//   const [results, setResults] = useState(null);
+
+//   useEffect(() => {
+//     fetch(
+//       "https://api.openweathermap.org/data/2.5/weather?q=" +
+//         city +
+//         "&units=metric" +
+//         "&appid=" +
+//         process.env.REACT_APP_APIKEY
+//     )
+//       .then((res) => res.json())
+//       .then(
+//         (result) => {
+//           if (result["cod"] !== 200) {
+//             setIsLoaded(false);
+//           } else {
+//             setIsLoaded(true);
+//             setResults(result);
+//           }
+//         },
+//         (error) => {
+//           setIsLoaded(true);
+//           setError(error);
+//         }
+//       );
+//   }, [city]);
+
+//   if (error) {
+//     return <div>Error: {error.message}</div>;
+//   } else {
+//     return (
+//       <>
+//         <img className="logo" src={logo} alt="MLH Prep Logo"></img>
+//         <div>
+//           <h2>Enter a city below 👇</h2>
+//           <input
+//             type="text"
+//             value={city}
+//             onChange={(event) => setCity(event.target.value)}
+//           />
+//           <div className="Results">
+//             {!isLoaded && <h2>Loading...</h2>}
+//             {console.log(results)}
+//             {isLoaded && results && (
+//               <>
+//                 <h3>{results.weather[0].main}</h3>
+//                 <p>Feels like {results.main.feels_like}°C</p>
+//                 <i>
+//                   <p>
+//                     {results.name}, {results.sys.country}
+//                   </p>
+//                 </i>
+//               </>
+//             )}
+//           </div>
+//         </div>
+//       </>
+//     );
+//   }
+// }
 function App() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [city, setCity] = useState("New York City")
   const [results, setResults] = useState(null);
+  const [location, setLocation] = useState("");
 
-  useEffect(() => {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + process.env.REACT_APP_APIKEY)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          if (result['cod'] !== 200) {
-            setIsLoaded(false)
-          } else {
-            setIsLoaded(true);
-            setResults(result);
-          }
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=";
+  const fetchWeatherData = () => {
+    axios
+      // .get(`${url}` + "tokyo" + "&units=metric" + `&appid=${REACT_APP_APIKEY}`)
+      .get(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=${REACT_APP_APIKEY}`
       )
-  }, [city])
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else {
-    return <>
-      <img className="logo" src={logo} alt="MLH Prep Logo"></img>
-      <div>
-        <h2>Enter a city below 👇</h2>
-        <input
-          type="text"
-          value={city}
-          onChange={event => setCity(event.target.value)} />
-        <div className="Results">
-          {!isLoaded && <h2>Loading...</h2>}
-          {console.log(results)}
-          {isLoaded && results && <>
-            <h3>{results.weather[0].main}</h3>
-            <p>Feels like {results.main.feels_like}°C</p>
-            <i><p>{results.name}, {results.sys.country}</p></i>
-          </>}
-        </div>
-      </div>
-    </>
-  }
+      .then((res) => {
+        const allData = res.data;
+        setResults(allData);
+        // console.log(allData);
+      })
+      .catch((err) => {
+        console.log(err.toJSON());
+      });
+  };
+  useEffect(() => {
+    fetchWeatherData();
+  }, []);
+  // return <div className="App">{<Result results={results} />}</div>;
+  return <Result results={results} />;
+  // return <div>test</div>;
 }
-
 export default App;
---
+++ src/Components/Result.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+
+const Result = (results) => {
+  console.log(results);
+  // return <div>{results.results.cod}</div>;
+  return <div>{results.results.cod}</div>;
+};
+
+export default Result;
