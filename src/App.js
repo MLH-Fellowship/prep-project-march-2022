c6299a84d9c109dddfbef29870402cd1eb43b58d
shivamsingh124 <shivamsingh12042018@gmail.com>
2022-03-19 02:20:39 +0530

    branchupdate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
 import { useEffect, useState } from "react";
 import './App.css';
 import logo from './mlh-prep.png'
-
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [city, setCity] = useState("New York City")
+  const [city, setCity] = useState("Globe")
   const [results, setResults] = useState(null);
 
+useEffect(() => {
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition,showError);
+    }
+    else{
+      window.alert("Geolocation is not supported by this browser.");
+    }
+
+function showPosition(position){
+  var lat=position.coords.latitude;
+  var lon=position.coords.longitude;
+  currentweather(lat,lon);
+}
+function showError(error){
+    switch(error.code){
+      case error.PERMISSION_DENIED:
+                window.alert("User denied the request for Geolocation.")
+              break;
+              case error.POSITION_UNAVAILABLE:
+                window.alert("Location information is unavailable.")
+              break;
+              case error.TIMEOUT:
+                window.alert(" The request to get user location timed out.")
+              break;
+              case error.UNKNOWN_ERROR:
+                window.alert("An unknown error occurred.")
+              break;
+    }
+}
+
+function currentweather(lat, lon){
+  fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon +"&appid=" + process.env.REACT_APP_APIKEY + "&units=metric") 
+      .then(res => res.json())
+      .then(
+        (result) => {
+          if (result['cod'] !== 200) {
+            setIsLoaded(false)
+          } else {
+            setIsLoaded(true);
+            setResults(result);
+            setCity(result.name);
+          }
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+}
+},[])
   useEffect(() => {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + process.env.REACT_APP_APIKEY)
       .then(res => res.json())
