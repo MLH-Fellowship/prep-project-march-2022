9f32f3694ec65b872c8cf38605be0bab849e3094
Kanchi Jain <jainkanchi2001@gmail.com>
2022-03-20 18:18:04 +0530

    resolved conflicts

--- src/components/HourlyForecast.js
+++ src/components/HourlyForecast.js
@@ -28,42 +28,18 @@ const responsive = {
 
 const HourlyForecast = ({results, lat, lon, city ,key}) => {
 
-console.log(results)
-console.log(lat)
-console.log(lon)
-console.log(city)
   const [data, setData] = useState(results);
   const [time, setTime] = useState(0);
-  console.log(data)
-  // let lat = 0
-  // let lon = 0
-  // console.log(lat+ " " + lon)
-
-  // console.log(results);
-  // console.log(results.lat)
-  // setLat(results.lat);
-  // setLon(results.lon)
-  console.log("-----------------------")
-  // console.log(lat);
-  // console.log("😎=  " + lon)
-  // console.log("-------------")
   useEffect(() => {
     fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=daily&appid="+process.env.REACT_APP_APIKEY)
         .then((res) => res.json())
         .then(
             (result) => {
                 if (result.cod !== "200") {
-                  //  console.log(result);
                   setData(result);
-                  //  console.log(data);
-                  console.log("😴😴")
-                   console.log("😍 = " + result.hourly[0].feels_like);
-                   console.log("😅🌟 = " + result.hourly[0].weather[0].icon);
                    const dt = result.hourly[0].dt;
                    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
                    var day = new Date(dt*1000);
-                   console.log(day.getUTCHours() + " UTC")
-                   console.log(days[day.getDay()])
                    setTime(day.getUTCHours() + " UTC")
                 } else {
                    console.log("Something is worng Error");
@@ -78,7 +54,6 @@ console.log(city)
 const days = { 0 : 'Sun', 1 : 'Mon', 2 : 'Tue', 3 : 'Wed', 4 : 'Thu', 5 : 'Fri', 6 : 'Sat' };
 const keys = Object.values(days);
 
-console.log(data);
   return (
     <div>
       <div className="component">
@@ -91,8 +66,7 @@ console.log(data);
             {new Date(data==null?0:(data.hourly[index].dt)*1000).getUTCHours() + " UTC"}
             </Typography>
             <Typography color="text.secondary">{keys[new Date(data==null?0:(data.hourly[index].dt)*1000).getDay()]}</Typography>
-            <Typography variant="h4">{data==null?0:(data.hourly[index].feels_like).toFixed(2)}° K</Typography>
-            <Typography variant="h4">{data==null?0:(data.hourly[index].weather[0].icon)+".png"}</Typography>
+            <Typography variant="h4">{data==null?0:((data.hourly[index].feels_like).toFixed(2))}° K</Typography>
             </CardContent>
         ))
         }
