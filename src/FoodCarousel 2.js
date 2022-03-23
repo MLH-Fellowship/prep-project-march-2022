63ff89ee0354b5e64e7547dc9de9de75de266ff4
Annica <45278655+chiuannica@users.noreply.github.com>
2022-03-23 09:35:17 +0800

    Merge conflicts

---
+++ src/FoodCarousel 2.js
@@ -0,0 +1,66 @@
+import react, { useEffect, useState, useRef } from "react";
+import FoodItem from "./foodItem";
+
+const delay = 2500;
+
+export default function FoodCarousel(props) {
+  const [index, setIndex] = useState(0);
+  const timeoutRef = useRef(null);
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === props.items.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
+
+  return (
+    <div className="slideshow">
+      <div
+        className="slideshowSlider"
+        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+      >
+        {props.items.map((fooditem, index) => {
+          return (
+            <div className="slide" key={index}>
+              <FoodItem
+                key={fooditem.id}
+                name={fooditem.title}
+                image={fooditem.image}
+                url={fooditem.sourceUrl}
+              />
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="slideshowDots">
+        {props.items.map((_, idx) => {
+          return (
+            <div
+              key={idx}
+              className={`slideshowDot${index === idx ? " active" : ""}`}
+              onClick={() => {
+                setIndex(idx);
+              }}
+            ></div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
