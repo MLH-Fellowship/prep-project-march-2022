19fd728f429f22d60ef8f2e91a081904cda14fc5
cyrilchukwuebuka <muofunanya3@gmail.com>
2022-03-23 07:27:51 +0100

    Implemented NewsAPI

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SearchBox from './components/SearchBox'
 import HourlyForecast from './components/HourlyForecast.js'
 import Suggestions from './components/suggestions/suggestions'
 import SongRecommendation from "./components/SongRecommendation/SongRecommendation";
+import NewsUpdate from "./components/newsUpdate/NewsUpdate";
 
 
 function App() {
@@ -22,109 +23,109 @@ function App() {
   const [results, setResults] = useState(null);
   // const [weatherCondition, setWeatherCondtion] = useState('')
   // const [token, setToken] = useState('')
-  const [lat , setLat] = useState(0.00);
-  const [lon , setLon] = useState(0.00);
+  const [lat, setLat] = useState(0.00);
+  const [lon, setLon] = useState(0.00);
 
   const [fooditems, setFooditems] = useState(null);
 
-  const change = (response) => {
-    let endpoint1 =
-      "https://api.spoonacular.com/recipes/complexSearch/?apiKey=" +
-      process.env.REACT_APP_FOODAPIKEY;
-    let endpoint2 = endpoint1;
-    let endpoint3 = endpoint1;
-    if (response.weather[0].main === "Rain") {
-      endpoint1 = endpoint1 + "&query=noodle,soup&number=5&sort=random";
-      endpoint2 = endpoint2 + "&query=pot&number=5&sort=random";
-      endpoint3 = endpoint3 + "&type=fingerfood&number=5&sort=random";
-    } else if (
-      response.weather[0].main === "Snow" ||
-      response.main.feels_like < 10
-    ) {
-      endpoint1 = endpoint1 + "&type=soup&number=5&sort=random";
-      endpoint2 = endpoint2 + "&query=mug&number=5&sort=random";
-      endpoint3 = endpoint3 + "&query=stroganoff&number=5&sort=random";
-    } else {
-      endpoint1 = endpoint1 + "&type=beverage&number=5&sort=random";
-      endpoint2 = endpoint2 + "&query=summer,salad&number=5&sort=random";
-      endpoint3 = endpoint3 + "&query=sorbet&number=5&sort=random";
-    }
+  // const change = (response) => {
+  //   let endpoint1 =
+  //     "https://api.spoonacular.com/recipes/complexSearch/?apiKey=" +
+  //     process.env.REACT_APP_FOODAPIKEY;
+  //   let endpoint2 = endpoint1;
+  //   let endpoint3 = endpoint1;
+  //   if (response.weather[0].main === "Rain") {
+  //     endpoint1 = endpoint1 + "&query=noodle,soup&number=5&sort=random";
+  //     endpoint2 = endpoint2 + "&query=pot&number=5&sort=random";
+  //     endpoint3 = endpoint3 + "&type=fingerfood&number=5&sort=random";
+  //   } else if (
+  //     response.weather[0].main === "Snow" ||
+  //     response.main.feels_like < 10
+  //   ) {
+  //     endpoint1 = endpoint1 + "&type=soup&number=5&sort=random";
+  //     endpoint2 = endpoint2 + "&query=mug&number=5&sort=random";
+  //     endpoint3 = endpoint3 + "&query=stroganoff&number=5&sort=random";
+  //   } else {
+  //     endpoint1 = endpoint1 + "&type=beverage&number=5&sort=random";
+  //     endpoint2 = endpoint2 + "&query=summer,salad&number=5&sort=random";
+  //     endpoint3 = endpoint3 + "&query=sorbet&number=5&sort=random";
+  //   }
 
-    Promise.all([
-      fetch(endpoint1 + "&addRecipeInformation=true").then((res) => res.json()),
-      fetch(endpoint2 + "&addRecipeInformation=true").then((res) => res.json()),
-      fetch(endpoint3 + "&addRecipeInformation=true").then((res) => res.json()),
-    ]).then(
-      (food) => {
-        if (food != null) {
-          let arr = food[0]["results"]
-            .concat(food[1]["results"])
-            .concat(food[2]["results"]);
-          setFooditems(arr);
-          console.log(fooditems);
-        } else {
-          console.log("No results found");
-        }
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  };
+  //   Promise.all([
+  //     fetch(endpoint1 + "&addRecipeInformation=true").then((res) => res.json()),
+  //     fetch(endpoint2 + "&addRecipeInformation=true").then((res) => res.json()),
+  //     fetch(endpoint3 + "&addRecipeInformation=true").then((res) => res.json()),
+  //   ]).then(
+  //     (food) => {
+  //       if (food != null) {
+  //         let arr = food[0]["results"]
+  //           .concat(food[1]["results"])
+  //           .concat(food[2]["results"]);
+  //         setFooditems(arr);
+  //         console.log(fooditems);
+  //       } else {
+  //         console.log("No results found");
+  //       }
+  //     },
+  //     (error) => {
+  //       console.log(error);
+  //     }
+  //   );
+  // };
 
 
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition, showError);
-    }
-    else {
-      window.alert("Geolocation is not supported by this browser.");
-    }
+  // useEffect(() => {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(showPosition, showError);
+  //   }
+  //   else {
+  //     window.alert("Geolocation is not supported by this browser.");
+  //   }
 
-    function showPosition(position) {
-      let lat = position.coords.latitude;
-      let lon = position.coords.longitude;
-      currentweather(lat, lon);
-    }
-    function showError(error) {
-      switch (error.code) {
-        case error.PERMISSION_DENIED:
-          window.alert("User denied the request for Geolocation.")
-          break;
-        case error.POSITION_UNAVAILABLE:
-          window.alert("Location information is unavailable.")
-          break;
-        case error.TIMEOUT:
-          window.alert(" The request to get user location timed out.")
-          break;
-        case error.UNKNOWN_ERROR:
-          window.alert("An unknown error occurred.")
-          break;
-        default:
-          window.alert('')
-      }
-    }
+  //   function showPosition(position) {
+  //     let lat = position.coords.latitude;
+  //     let lon = position.coords.longitude;
+  //     currentweather(lat, lon);
+  //   }
+  //   function showError(error) {
+  //     switch (error.code) {
+  //       case error.PERMISSION_DENIED:
+  //         window.alert("User denied the request for Geolocation.")
+  //         break;
+  //       case error.POSITION_UNAVAILABLE:
+  //         window.alert("Location information is unavailable.")
+  //         break;
+  //       case error.TIMEOUT:
+  //         window.alert(" The request to get user location timed out.")
+  //         break;
+  //       case error.UNKNOWN_ERROR:
+  //         window.alert("An unknown error occurred.")
+  //         break;
+  //       default:
+  //         window.alert('')
+  //     }
+  //   }
 
-    function currentweather(lat, lon) {
-      fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + process.env.REACT_APP_APIKEY + "&units=metric")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            if (result['cod'] !== 200) {
-              setIsLoaded(false)
-            } else {
-              setIsLoaded(true);
-              setResults(result);
-              setCity(result.name);
-            }
-          },
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        )
-    }
-  }, [])
+  //   function currentweather(lat, lon) {
+  //     fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + process.env.REACT_APP_APIKEY + "&units=metric")
+  //       .then(res => res.json())
+  //       .then(
+  //         (result) => {
+  //           if (result['cod'] !== 200) {
+  //             setIsLoaded(false)
+  //           } else {
+  //             setIsLoaded(true);
+  //             setResults(result);
+  //             setCity(result.name);
+  //           }
+  //         },
+  //         (error) => {
+  //           setIsLoaded(true);
+  //           setError(error);
+  //         }
+  //       )
+  //   }
+  // }, [])
 
   // useEffect(() => {
   //   const getToken = async () => {
@@ -157,7 +158,7 @@ function App() {
             // setWeatherCondtion(results?.weather[0]?.main)
             setLat(result.coord.lat);
             setLon(result.coord.lon)
-            change(result);
+            // change(result);
           }
         },
         (error) => {
@@ -172,46 +173,45 @@ function App() {
   } else {
     return (
       <>
-      <img className="logo" src={logo} alt="MLH Prep Logo"></img>
-      <div>
-        <h2>Enter a city below 👇</h2>
+        <img className="logo" src={logo} alt="MLH Prep Logo"></img>
+        <div>
+          <h2>Enter a city below 👇</h2>
 
-          <SearchBox 
+          <SearchBox
             setCity={setCity}
           />
 
-        <div className="Results">
-          {!isLoaded && <h2>Loading...</h2>}
-          {isLoaded && results && <>
-            <h3>{results.weather[0].main}</h3>
-            <p>Feels like {results.main.feels_like}°C</p>
-            <i><p>{results.name}, {results.sys.country}</p></i>
-          </>}
+          <div className="Results">
+            {!isLoaded && <h2>Loading...</h2>}
+            {isLoaded && results && <>
+              <h3>{results.weather[0].main}</h3>
+              <p>Feels like {results.main.feels_like}°C</p>
+              <i><p>{results.name}, {results.sys.country}</p></i>
+            </>}
+          </div>
+          {city && <NewsUpdate city={city} />}
+          {/* {weatherCondition && <Playlist city={city} weatherCondition={weatherCondition} token={token}/>} */}
+          {/* {isLoaded && results &&
+            <Suggestions
+              weather={results.weather[0].main}
+            />
+          }
         </div>
-        {/* {weatherCondition && <Playlist city={city} weatherCondition={weatherCondition} token={token}/>} */}
-        {isLoaded && results &&
-          <Suggestions
-            weather={results.weather[0].main}
-          />
-        }
-        </div>
-      <div>
-        {/* <HourlyForecast results = {results} /> */}
-        <HourlyForecast results = {results} lat = {lat} lon = {lon}  city={city} key ={1}/>
-      </div>
         <div>
+          <HourlyForecast results={results} lat={lat} lon={lon} city={city} key={1} />
+        </div> */}
+        {/* <div>
             {isLoaded && results && <>
             <SongRecommendation options={results} />
             </>}
-        </div>
-        <div className="food-recommendations">
+        </div> */}
+        {/* <div className="food-recommendations">
           <h2 className="food-recommendations-title">
             Hungry? Here's some food you may like 😋
           </h2>
 
-          {fooditems && <FoodCarousel items={fooditems} />}
-        </div>
-
+          {fooditems && <FoodCarousel items={fooditems} />}*/}
+        </div> 
       </>
     );
   }
---
+++ src/components/newsUpdate/NewsCard.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import './../../styles/NewsCard.css'
+import Dataset from 'data.js'
+
+const NewsCard = ({ info }) => {
+    console.log(info)
+    const [date, setDate] = useState('')
+
+    const getDate = () => {
+        let newDate = Intl.DateTimeFormat().format(
+            new Date(info.publishedAt.toString())
+        )
+        console.log(newDate)
+        setDate(newDate)
+    }
+
+    useEffect(() => {
+        getDate()
+    }, [info])
+    
+    return (
+        <div className='news_card'>
+            <div className='news_card_image'>
+                <img alt='' src={info.urlToImage} />
+            </div>
+            <div className='news_card_body'>
+                <a href={info.url}>{info.title}</a>
+                <span>{date}</span>
+            </div>
+        </div>
+    )
+}
+
+export default NewsCard
