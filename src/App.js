53dd0f0489a2d75f9df830418206ecc6536321f5
Siddharth Nair <siddharthmani2000@gmail.com>
2022-03-24 21:10:58 +0530

    Alerts cleanup

--- src/App.js
+++ src/App.js
@@ -35,48 +35,48 @@ function App() {
   const [fooditems, setFooditems] = useState(null);
 
   const change = (response) => {
-    // let endpoint1 =
-    //   "https://api.spoonacular.com/recipes/complexSearch/?apiKey=" +
-    //   process.env.REACT_APP_FOODAPIKEY;
-    // let endpoint2 = endpoint1;
-    // let endpoint3 = endpoint1;
-    // if (response.weather[0].main === "Rain") {
-    //   endpoint1 = endpoint1 + "&query=noodle,soup&number=5&sort=random";
-    //   endpoint2 = endpoint2 + "&query=pot&number=5&sort=random";
-    //   endpoint3 = endpoint3 + "&type=fingerfood&number=5&sort=random";
-    // } else if (
-    //   response.weather[0].main === "Snow" ||
-    //   response.main.feels_like < 10
-    // ) {
-    //   endpoint1 = endpoint1 + "&type=soup&number=5&sort=random";
-    //   endpoint2 = endpoint2 + "&query=mug&number=5&sort=random";
-    //   endpoint3 = endpoint3 + "&query=stroganoff&number=5&sort=random";
-    // } else {
-    //   endpoint1 = endpoint1 + "&type=beverage&number=5&sort=random";
-    //   endpoint2 = endpoint2 + "&query=summer,salad&number=5&sort=random";
-    //   endpoint3 = endpoint3 + "&query=sorbet&number=5&sort=random";
-    // }
+    let endpoint1 =
+      "https://api.spoonacular.com/recipes/complexSearch/?apiKey=" +
+      process.env.REACT_APP_FOODAPIKEY;
+    let endpoint2 = endpoint1;
+    let endpoint3 = endpoint1;
+    if (response.weather[0].main === "Rain") {
+      endpoint1 = endpoint1 + "&query=noodle,soup&number=5&sort=random";
+      endpoint2 = endpoint2 + "&query=pot&number=5&sort=random";
+      endpoint3 = endpoint3 + "&type=fingerfood&number=5&sort=random";
+    } else if (
+      response.weather[0].main === "Snow" ||
+      response.main.feels_like < 10
+    ) {
+      endpoint1 = endpoint1 + "&type=soup&number=5&sort=random";
+      endpoint2 = endpoint2 + "&query=mug&number=5&sort=random";
+      endpoint3 = endpoint3 + "&query=stroganoff&number=5&sort=random";
+    } else {
+      endpoint1 = endpoint1 + "&type=beverage&number=5&sort=random";
+      endpoint2 = endpoint2 + "&query=summer,salad&number=5&sort=random";
+      endpoint3 = endpoint3 + "&query=sorbet&number=5&sort=random";
+    }
 
-    // Promise.all([
-    //   fetch(endpoint1 + "&addRecipeInformation=true").then((res) => res.json()),
-    //   fetch(endpoint2 + "&addRecipeInformation=true").then((res) => res.json()),
-    //   fetch(endpoint3 + "&addRecipeInformation=true").then((res) => res.json()),
-    // ]).then(
-    //   (food) => {
-    //     if (food != null) {
-    //       let arr = food[0]["results"]
-    //         .concat(food[1]["results"])
-    //         .concat(food[2]["results"]);
-    //       setFooditems(arr);
-    //       console.log(fooditems);
-    //     } else {
-    //       console.log("No results found");
-    //     }
-    //   },
-    //   (error) => {
-    //     console.log(error);
-    //   }
-    // );
+    Promise.all([
+      fetch(endpoint1 + "&addRecipeInformation=true").then((res) => res.json()),
+      fetch(endpoint2 + "&addRecipeInformation=true").then((res) => res.json()),
+      fetch(endpoint3 + "&addRecipeInformation=true").then((res) => res.json()),
+    ]).then(
+      (food) => {
+        if (food != null) {
+          let arr = food[0]["results"]
+            .concat(food[1]["results"])
+            .concat(food[2]["results"]);
+          setFooditems(arr);
+          console.log(fooditems);
+        } else {
+          console.log("No results found");
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   };
 
 
