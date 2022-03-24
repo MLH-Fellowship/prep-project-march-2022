df0cc2bb90461d791ae0f56c5f3a98b0574b8252
Chibi-girl <lucysanzenin@gmail.com>
2022-03-24 20:27:43 +0530

    Added toggle display feature

--- src/App.js
+++ src/App.js
@@ -14,8 +14,24 @@ const App = () => {
   const [results, setResults] = useState(null);
   const [lat, setLat] = useState(0.0);
   const [lon, setLon] = useState(0.0);
+  const [disp, setDisp] = useState("Cel");
+  const [feels_like,setFeelslike] = useState(null);
   const [fooditems, setFooditems] = useState(null);
-
+  
+   const handleChange = () => {
+  if(disp==="Cel")
+  {
+  	setDisp("Fah");
+  	setFeelslike(((results.main.feels_like*1.8)+32)+" \xB0 F");
+  }
+  else
+  {
+  	setDisp("Cel");
+  	setFeelslike(results.main.feels_like+" \xB0 C");
+  }
+  }
+  
+  
   const change = (response) => {
     let endpoint1 = `https://api.spoonacular.com/recipes/complexSearch/?apiKey=${process.env.REACT_APP_FOOD_API_KEY}`;
     let endpoint2 = endpoint1;
@@ -128,6 +144,7 @@ const App = () => {
             setResults(result);
             setLat(result.coord.lat);
             setLon(result.coord.lon);
+            setFeelslike(result.main.feels_like+" \xB0 C");
             change(result);
           }
         },
@@ -155,12 +172,17 @@ const App = () => {
             {isLoaded && results && (
               <>
                 <h3>{results.weather[0].main}</h3>
-                <p>Feels like {results.main.feels_like}°C</p>
+                <p>Feels like {feels_like}</p>
                 <i>
                   <p>
                     {results.name}, {results.sys.country}
                   </p>
                 </i>
+                 <p>Toggle temperature display</p>
+                 <label class="switch">
+				  <input type="checkbox" onChange={handleChange}/>
+				  <span class="slider round"></span>
+				</label>
               </>
             )}
           </div>
