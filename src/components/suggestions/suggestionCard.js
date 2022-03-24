4b1df94464cc2747d4cecc195145850f190f69b8
Neha <goyalneha2607@gmail.com>
2022-03-24 15:49:10 +0530

    Suggestion card formatting fixed

--- src/components/suggestions/suggestionCard.js
+++ src/components/suggestions/suggestionCard.js
@@ -5,12 +5,12 @@ function SuggestionCard({item}){
     const file = require('../../assets/lottiefiles/'+ item.toLowerCase() + '.json')
 
     return (
-        <div className="card">
-            <div className="card-img">
+        <div className="suggestion-card">
+            <div className="suggestion-card-img">
                 <LottieControl animationData={file} />
             </div>
             
-            <h2 className="primary-text"> {item} <br/></h2>
+            <h2 className="suggestion-primary-text"> {item} <br/></h2>
         </div>
     )
 }
