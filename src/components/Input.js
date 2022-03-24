7afca73ac366d1218399c307cdae0f7c7a0df6f8
RWGregory <91198091+RWGregory@users.noreply.github.com>
2022-03-24 11:49:58 -0400

    input type back to search

--- src/components/Input.js
+++ src/components/Input.js
@@ -24,7 +24,7 @@ export default function Input({city, setCity, setLatLng, results, clickedLast, s
             <h2>Enter a city below 👇</h2>
             <input
                 id="city"
-                type="text"
+                type="search"
                 value={clickedLast ? results.name : city}
                 onChange={event => {
                     setCity(event.target.value)  
